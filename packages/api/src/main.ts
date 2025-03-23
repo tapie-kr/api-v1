33ef3756385071a96c39fae4d5091fa676a2456d
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-03-24 00:36:41 +0900

    refactor(main): streamline imports and improve formatting in main.ts

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,14 +1,14 @@
+import { AppModule } from '@/app.module';
+import { APIResponseDto } from '@/common/dto/response.dto';
+import { GlobalExceptionFilter } from '@/common/filters/global-exception.filter';
+import { PrismaExceptionFilter } from '@/common/filters/prisma-exception.filter';
+import { TransformInterceptor } from '@/common/interceptors/transform.interceptor';
 import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
-import { AppModule } from '@/app.module';
-import { APIResponseDto } from '@/common/dto/response.dto';
-import { GlobalExceptionFilter } from '@/common/filters/global-exception.filter';
-import { PrismaExceptionFilter } from '@/common/filters/prisma-exception.filter';
-import { TransformInterceptor } from '@/common/interceptors/transform.interceptor';
 
 import './instrument';
 
@@ -22,17 +22,19 @@ async function bootstrap() {
     logger.log('Running in production mode');
   }
 
-  app.useGlobalFilters(new PrismaExceptionFilter);
+  app.useGlobalFilters(new PrismaExceptionFilter());
 
-  app.useGlobalFilters(new GlobalExceptionFilter);
+  app.useGlobalFilters(new GlobalExceptionFilter());
 
-  app.useGlobalInterceptors(new TransformInterceptor);
+  app.useGlobalInterceptors(new TransformInterceptor());
 
-  app.useGlobalPipes(new ValidationPipe({
-    transform:            true,
-    whitelist:            true,
-    forbidNonWhitelisted: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   app.use(cookieParser());
 
@@ -43,63 +45,65 @@ async function bootstrap() {
       'https://admin.tapie.kr',
       'https://auth.tapie.kr',
     ],
-    methods: [
-      'GET',
-      'POST',
-      'PUT',
-      'DELETE',
-      'PATCH',
-    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
 
   app.enableVersioning({
-    type:           VersioningType.URI,
+    type: VersioningType.URI,
     defaultVersion: '1',
   });
 
   // if (!isProduction) {
   if (true) {
-    const theme = new SwaggerTheme;
+    const theme = new SwaggerTheme();
 
-    const config = (new DocumentBuilder)
+    const config = new DocumentBuilder()
       .setTitle('TAPIE API')
       .setDescription('TAPIE System API')
-      .addCookieAuth('accessToken',
+      .addCookieAuth(
+        'accessToken',
         {
           type: 'apiKey',
-          in:   'cookie',
+          in: 'cookie',
         },
-        'accessToken')
-      .addCookieAuth('refreshToken',
+        'accessToken',
+      )
+      .addCookieAuth(
+        'refreshToken',
         {
           type: 'apiKey',
-          in:   'cookie',
+          in: 'cookie',
         },
-        'refreshToken')
+        'refreshToken',
+      )
       .build();
 
-    const document = SwaggerModule.createDocument(app, config, { extraModels: [APIResponseDto] });
+    const document = SwaggerModule.createDocument(app, config, {
+      extraModels: [APIResponseDto],
+    });
 
     SwaggerModule.setup('api-docs', app, document, {
       jsonDocumentUrl: 'api-docs/json',
-      explorer:        true,
+      explorer: true,
       yamlDocumentUrl: 'api-docs/yaml',
-      customCss:       theme.getBuffer(SwaggerThemeNameEnum.ONE_DARK),
-      customfavIcon:   'https://s3.vport.dev/tapie-static/favicon.ico',
+      customCss: theme.getBuffer(SwaggerThemeNameEnum.ONE_DARK),
+      customfavIcon: 'https://s3.tapie.kr/tapie-static/favicon.ico',
       customSiteTitle: 'TAPIE API Swagger',
     });
   }
 
   app.getHttpAdapter().get('/', (_req, res) => {
     res.json({
-      status:            'ok',
+      status: 'ok',
       availableVersions: ['v1'],
     });
   });
 
-  await app.listen(isProduction ? Number(configService.get('PORT') || 3000) : 8877,
-    '0.0.0.0');
+  await app.listen(
+    isProduction ? Number(configService.get('PORT') || 3000) : 8877,
+    '0.0.0.0',
+  );
 
   logger.log(`Server running on ${await app.getUrl()}`);
 }
